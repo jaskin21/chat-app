e218ff42a9142dfa79c710f0fde530c4271934d7
jaskin21 <jaskin.agmata@gmail.com>
2023-02-26 02:04:29 +0800

    feat: added navigate to login if unauthorize

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Profile from './components/Profile';
+import UserForm from './components/UserForm';
+import Chat from './components/Chat';
 import NotFound from './pages/NotFound';
+import withAuth from './utils/withAuth';
 
 import './styles/index.css';
 // import Home from './pages/Home';
@@ -11,7 +14,9 @@ function App() {
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route index element={<Home />} path='/' />
+          <Route index element={<UserForm />} path='/' />
+          <Route element={withAuth(<Profile />)} path='/profile' />
+          <Route element={withAuth(<Chat />)} path='/chat' />
           <Route element={<NotFound />} path='*' />
         </Routes>
       </div>
