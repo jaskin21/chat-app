660499d25f2e464177c51ee1298bea441af9cc22
jaskin21 <jaskin.agmata@gmail.com>
2023-02-27 21:37:59 +0800

    feat: added logout

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import UseToken from '../utils/UseToken';
 
 const Profile = () => {
+  const { setToken } = UseToken();
+
+  // function to handle logout
+  const handleLogout = () => {
+    // remove the token from localStorage
+    localStorage.removeItem('token');
+    // update token value
+    setToken();
+  };
+
   return (
     <main className='main-container'>
       <section className='forms-section'>
         <h1>Hi! Jaskin</h1>
         <div>
-          <Link>Logout</Link>
+          <Link onClick={handleLogout}>Logout</Link>
           <Link to='/chat'>Chat</Link>
         </div>
       </section>
